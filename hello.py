7e1bf5df1b47f3fc803644f9e62703250877f6a5
Tim Walsh <walsh416@gmail.com>
2017-10-11 11:22:38 -0400

    Basic login and register pages
    No information sent back and forth yet

--- hello.py
+++ hello.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return '''
-Hello World!
-<a href="/foo">Click for foo!</a>
+<h1>Welcome to isThere.today!</h1>
+Click below to register or login.<br>
+<a href="/register">Register</a><br>
+<a href="/login">Login</a> 
 '''
+
+@app.route("/login")
+def login():
+        return render_template('login.html')
+
+@app.route("/register")
+def register():
+        return render_template('register.html')
 
 @app.route("/<username>")
 def bar(username):
-	return render_template('userTemplate.html',
-				name=username)
+        return render_template('userTemplate.html',
+                                name=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
