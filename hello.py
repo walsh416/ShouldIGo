de4a33bed0bdc773a8784cb81b6b5961a23cfca2
Tim Walsh <walsh416@gmail.com>
2017-10-11 10:44:09 -0400

    Basic Flask stuff implemented
    Navigate back and forth, etc.  Still blank Get Data button, and not sure if the {% if name %} part of the userTemplate is actually working...

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return '''
+Hello World!
+<a href="/foo">Click for foo!</a>
+'''
 
-@app.route("/foo")
-def bar():
-	return "Foo bar!"
+@app.route("/<username>")
+def bar(username):
+	return render_template('userTemplate.html',
+				name=username)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
