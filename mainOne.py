e013a99b82f85a66520c226be7c7ab9ce2ea47d9
Tim Walsh <walsh416@gmail.com>
2017-10-13 16:10:33 -0400

    Add MySQL support
    Runs off of a MySQL db called "foo_db", that has a table of usernames and passwords.
    
    http://localhost:5000/Authenticate?UserName=FOO&Password=BAR shows "logged in successfully" if username FOO and password BAR are both in the table, and "username or password is wrong" if they don't match.
    
    Cited: http://codehandbook.org/python-web-application-flask-mysql/

--- mainOne.py
+++ mainOne.py
@@ -1,6 +1,15 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'foo_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def hello():
@@ -13,6 +22,19 @@ Click below to register or login.<br>
 <a href="/register">Register</a><br>
 <a href="/login">Login</a> 
 '''
+
+@app.route("/Authenticate")
+def Authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+     return "Username or Password is wrong"
+    else:
+     return "Logged in successfully"
+
 
 @app.route("/login")
 def login():
