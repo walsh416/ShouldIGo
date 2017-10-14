6d8f7d500fd1e5637aaaba69d6a3c332be0eb46f
Tim Walsh <walsh416@gmail.com>
2017-10-14 00:57:03 -0400

    Start working on POST to database
    Only on register page, nothing on login.
    
    Errors out when hitting submit... MySQL error?

--- mainOne.py
+++ mainOne.py
@@ -1,17 +1,20 @@
 
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
+from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 
 mysql = MySQL()
 app = Flask(__name__)
+api = Api(app)
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'foo_db'
+app.config['MYSQL_DATABASE_DB'] = 'userDb'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def hello():
     return '''
 <head>
@@ -35,12 +38,42 @@ def Authenticate():
     else:
      return "Logged in successfully"
 
+class HandleRegister(Resource):
+	def post(self):
+		try:
+			parser = reqparse.RequestParser()
+            		parser.add_argument('email', type=str, help='Email address to create user')
+            		parser.add_argument('password', type=str, help='Password to create user')
+            		args = parser.parse_args()
+
+            		_userEmail = args['email']
+            		_userPassword = args['password']
+
+            		conn = mysql.connect()
+            		cursor = conn.cursor()
+            		cursor.callproc('spCreateUser',(_userEmail,_userPassword))
+            		data = cursor.fetchall()
+
+            		if len(data) is 0:
+                		conn.commit()
+                		return {'StatusCode':'200','Message': 'User creation success'}
+            		else:
+            			return {'StatusCode':'1000','Message': str(data[0])}
+
+        	except Exception as e:
+            		return {'error': str(e)}
+
+api.add_resource(HandleRegister, '/HandleRegister')
+
+# @app.route("/handleregister", methods=["GET","POST"])
+# def handle_reg():
+# 	return request.form["firstname"] request.form['lastname'] + request.form['username']
 
 @app.route("/login")
 def login():
         return render_template('login.html')
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
         return render_template('register.html')
 
