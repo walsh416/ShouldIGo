de0d43f6915830f402c27db0d4bc1f9c759015b4
Tim Walsh <walsh416@gmail.com>
2017-10-14 17:08:45 -0400

    Ugh

--- mainOne.py
+++ mainOne.py
@@ -1,10 +1,6 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
-<<<<<<< HEAD
-=======
-
->>>>>>> jacob-branch
 app = Flask(__name__)
 
 mysql = MySQL()
@@ -32,7 +28,6 @@ cursorTemp.execute(out)
 connectionTemp.commit()
 connectionTemp.close()
 
-<<<<<<< HEAD
 def hash(rawpassword):
 	salt = "5gz"
 	db_password = rawpassword+salt
@@ -42,21 +37,6 @@ def hash(rawpassword):
 @app.route("/", methods=["GET","POST"])
 def hello():
     return render_template('splashScreen.html')
-=======
-
-@app.route("/", methods=["GET","POST"])
-def hello():
-    return '''
-<head>
-        <link rel= "stylesheet" type= "text/css" href= "{{ url_for('static',filename='styles/style.css') }}" />
-</head>
-<h1>Welcome to isThere.today!</h1>
-Click below to register or login.<br>
-<a href="/register">Register</a><br>
-<a href="/login">Login</a>
-'''
-
->>>>>>> jacob-branch
 
 @app.route("/HandleRegister", methods=["GET","POST"])
 def handle_reg():
@@ -64,7 +44,6 @@ def handle_reg():
         # return redirect("/registerfail")
         return render_template('register.html', diffPasswords=True, duplicateUser=False)
     else:
-<<<<<<< HEAD
         try:
             connection = mysql.connect()
             cursor = connection.cursor()
@@ -85,39 +64,6 @@ def handle_reg():
             # TODO: make sure Exception e is 1062 duplicate entry?
             return render_template('register.html', diffPasswords=False, duplicateUser=True)
 
-=======
-        connection = mysql.connect()
-        cursor = connection.cursor()
-
-        _userFirstname = request.form['firstname']
-        _userLastname = request.form['lastname']
-        _userUsername = request.form['username']
-        _userPassword = request.form['password']
-
-        out = "INSERT INTO User values(\'" + _userFirstname + "\',\'" + _userLastname + "\',\'" + _userUsername + "\',\'" + _userPassword + "\')"
-        cursor.execute(out)
-        connection.commit()
-        return redirect("/login")
-
-@app.route("/registerfail", methods = ["GET","POST"])
-def registerfail():
-    return render_template('registerwrong.html')
-
-@app.route("/loginCheck", methods = ["GET", "POST"])
-def loginCheck():
-    username = request.form['username']
-    password = request.form['password']
-    
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from user where username='" + username + "' and password='" + password + "'")
-    data = cursor.fetchone()
-    if data is None:
-        return "Username or Password is wrong"
-    else:
-        return "Logged in successfully"
-    return render_template('login.html')
-    
->>>>>>> jacob-branch
 @app.route("/login")
 def login():
     return render_template('login.html')
@@ -126,25 +72,13 @@ def login():
 def register():
     return render_template('register.html', diffPasswords=False, duplicateUser=False)
 
-<<<<<<< HEAD
 # @app.route("/user", methods=["GET","POST"])
 # def helloUser():
 #     return render_template('helloUser.html', )
-=======
-# @app.route("/<username>")
-# def bar(username):
-#         return render_template('userTemplate.html',
-#                                 name=username)
-# foo
->>>>>>> jacob-branch
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> jacob-branch
 #####################
 ## mySql commands: ##
 #####################
