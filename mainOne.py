db73eca9365eb676fe1e9d94441a7291d8eb8629
Tim Walsh <walsh416@gmail.com>
2017-10-21 16:31:56 -0400

    Login/out works.  Database is now persistent

--- mainOne.py
+++ mainOne.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from flaskext.mysql import MySQL
 import hashlib, os
 
@@ -14,28 +14,30 @@ app.config['MYSQL_DATABASE_DB'] = 'userDb'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-connectionTemp = mysql.connect()
-cursorTemp = connectionTemp.cursor()
-out = '''DROP database IF EXISTS userDb;
-CREATE DATABASE userDb;
-USE userDb;
-CREATE TABLE User(
-firstname VARCHAR(50) NOT NULL,
-lastname VARCHAR(50) NOT NULL,
-username VARCHAR(50) NOT NULL,
-password VARCHAR(80) NOT NULL,
-salt VARCHAR(80) NOT NULL,
-ownedEventsCSV VARCHAR(200),
-primary key(username)
-);
-CREATE TABLE Event(
-eventURL VARCHAR(50) NOT NULL,
-ownersCSV VARCHAR(200) NOT NULL,
-primary key(eventURL)
-);'''
-cursorTemp.execute(out)
-connectionTemp.commit()
-connectionTemp.close()
+# commented out all the stuff to create a new database each time:
+######################################
+# connectionTemp = mysql.connect()
+# cursorTemp = connectionTemp.cursor()
+# # out = '''DROP database IF EXISTS userDb;
+# out = '''USE userDb;
+# CREATE TABLE User(
+# firstname VARCHAR(50) NOT NULL,
+# lastname VARCHAR(50) NOT NULL,
+# username VARCHAR(50) NOT NULL,
+# password VARCHAR(80) NOT NULL,
+# salt VARCHAR(80) NOT NULL,
+# ownedEventsCSV VARCHAR(200),
+# primary key(username)
+# );
+# CREATE TABLE Event(
+# eventURL VARCHAR(50) NOT NULL,
+# ownersCSV VARCHAR(200) NOT NULL,
+# primary key(eventURL)
+# );'''
+# cursorTemp.execute(out)
+# connectionTemp.commit()
+# connectionTemp.close()
+######################################
 
 def get_salt():
 	return str(hashlib.md5(os.urandom(64).encode("base-64")).hexdigest())
@@ -46,44 +48,41 @@ def hash_pass(rawpassword):
 
 @app.route("/", methods=["GET","POST"])
 def splashScreen():
-    return render_template('splashScreen.html')
+	_username = request.cookies.get('username')
+	if _username:
+		cursor = mysql.connect().cursor()
+		cursor.execute("SELECT * from User where username='" + _username + "'")
+		data = cursor.fetchone()
+		if data is None:
+			return render_template('login.html', badUser=True, badPass=False)
+		_firstname = data[0]
+		_lastname = data[1]
+		_ownedEvents=data[5]
 
-@app.route("/HandleRegister", methods=["GET","POST"])
-def handle_reg():
-    if request.form['password'] != request.form['passwordconfirm']:
-        # return redirect("/registerfail")
-        return render_template('register.html', diffPasswords=True, duplicateUser=False)
-    else:
-        try:
-			connection = mysql.connect()
-			cursor = connection.cursor()
-			_userFirstname = request.form['firstname']
-			_userLastname = request.form['lastname']
-			_userUsername = request.form['username']
-			_userSalt = get_salt()
-			_userPassword = hash_pass(request.form['password'] + _userSalt)
-			out = "INSERT INTO User values(\'" + _userFirstname + "\',\'" + _userLastname + "\',\'" + _userUsername + "\',\'" + _userPassword + "\',\'" + _userSalt + "\',\'\')"
-			cursor.execute(out)
-			connection.commit()
-			return render_template('userHome.html', username=_userUsername, firstname=_userFirstname, lastname=_userLastname)
-        except Exception as e:
-            print e;
-            # TODO: make sure Exception e is 1062 duplicate entry?
-            return render_template('register.html', diffPasswords=False, duplicateUser=True)
+		resp = make_response(render_template('userHome.html', username=_username, firstname=_firstname, lastname=_lastname, ownedEvents=_ownedEvents))
+		resp.set_cookie('username', _username)
+		return resp
+	else:
+		print "no username!"
+		return render_template('splashScreen.html')
+
+@app.route("/logout")
+def logout():
+	# this one errors out, it doesn't like url_for("/")
+	# resp = make_response(redirect(url_for("/index")))
+	# but this one should be legal:
+	resp = make_response(redirect("/"))
+	resp.set_cookie('username', '', expires=0)
+	return resp
 
 @app.route("/login", methods = ["GET","POST"])
 def login():
 	if request.method == "POST":
 		_username = request.form['username']
 
-		# TODO: get first, last name, etc from DB
-
 		cursor = mysql.connect().cursor()
 		cursor.execute("SELECT * from User where username='" + _username + "'")
 		data = cursor.fetchone()
-		# print "Hash pass in: " + _hashPassIn
-		# print "Hash pass out: " + data[3]
-		# print "data out: %s" % (data,)
 		if data is None:
 			return render_template('login.html', badUser=True, badPass=False)
 		_firstname = data[0]
@@ -93,10 +92,13 @@ def login():
 		_ownedEvents=data[5]
 
 		_hashPassIn = hash_pass(request.form['password'] + _saltOut)
-
 		if _hashPassIn != _hashPassOut:
 			return render_template('login.html', badUser=False, badPass=True)
-		return render_template('userHome.html', username=_username, firstname=_firstname, lastname=_lastname, ownedEvents=_ownedEvents)
+		# return render_template('userHome.html', username=_username, firstname=_firstname, lastname=_lastname, ownedEvents=_ownedEvents)
+
+		resp = make_response(render_template('userHome.html', username=_username, firstname=_firstname, lastname=_lastname, ownedEvents=_ownedEvents))
+		resp.set_cookie('username', _username)
+		return resp
 	else:
 	    return render_template('login.html', badlogin=False)
 
@@ -118,7 +120,11 @@ def register():
 				out = "INSERT INTO User values(\'" + _userFirstname + "\',\'" + _userLastname + "\',\'" + _userUsername + "\',\'" + _userPassword + "\',\'" + _userSalt + "\',\'\')"
 				cursor.execute(out)
 				connection.commit()
-				return render_template('userHome.html', username=_userUsername, firstname=_userFirstname, lastname=_userLastname)
+				# return render_template('userHome.html', username=_userUsername, firstname=_userFirstname, lastname=_userLastname)
+
+	   			resp = make_response(render_template('userHome.html', username=_userUsername, firstname=_userFirstname, lastname=_userLastname))
+				resp.set_cookie('username', _userUsername)
+				return resp
 			except Exception as e:
 				print e;
 				# TODO: make sure Exception e is 1062 duplicate entry?
@@ -138,17 +144,3 @@ def createEvent():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#####################
-## mySql commands: ##
-#####################
-# DROP database IF EXISTS userDb;
-# CREATE DATABASE userDb;
-# USE userDb;
-# CREATE TABLE User(
-# firstname VARCHAR(50) NOT NULL,
-# lastname VARCHAR(50) NOT NULL,
-# username VARCHAR(50) NOT NULL,
-# password VARCHAR(40) NOT NULL,
-# primary key(username)
-# );
