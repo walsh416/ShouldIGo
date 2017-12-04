8878db8051e6a6f561bbb61e2b794f96213b4450
robertgerami <31670940+robertgerami@users.noreply.github.com>
2017-12-04 11:32:41 -0500

    eek

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -12,9 +13,11 @@ MAIL_USE_TLS=False
 # MAIL_PORT=587,
 # MAIL_USE_SSL=False,
 # MAIL_USE_TSL=True,
-MAIL_USERNAME = 'timsemailforlols@gmail.com'
+# MAIL_USERNAME = 'timsemailforlols@gmail.com'
+MAIL_USERNAME = 'shouldigotoday@gmail.com'
 # Random one-off passphrase from Google
-MAIL_PASSWORD = 'vqlavnjpsmsytbtx'
+# MAIL_PASSWORD = 'vqlavnjpsmsytbtx'
+MAIL_PASSWORD = 'defaropdhempnyun'
 
 # Needed to enable session storage
 SECRET_KEY = "}M\xfc;.\xa0?\x99\xbb;\xe8\x96\xca7\x04\xc2K\x06\xb1\xc0JOeoE\xd9\xddz\x81\x16\xa1\x00"
@@ -29,10 +32,14 @@ SECRET_KEY = "}M\xfc;.\xa0?\x99\xbb;\xe8\x96\xca7\x04\xc2K\x06\xb1\xc0JOeoE\xd9\
 ### SSLify doesn't run if DEBUG = True, which is good because it doesn't work on localhost
 ################### IF RUNNING LOCALLY: ################
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/userDb"
+# SQLALCHEMY_DATABASE_URI = "mysql://root:localhost/userDb"
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/userDb"
+SQLALCHEMY_DATABASE_URI = "mysql://root:@127.0.0.1/userDb"
+
 #####################################################
 
 ################### IF RUNNING ON AWS: #################
 # DEBUG = False
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://walsh416:walsh416pass@walsh416dbinstance.cuo7hfdlcl5g.us-east-2.rds.amazonaws.com/thedatabase"
 #####################################################
+>>>>>>> 165424d1fbfc571132f1d76db9cf57be0848cfa2
